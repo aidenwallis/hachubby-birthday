ab8acdfdc763c3bcdf2c0a91405823176032a607
Aiden Wallis <aiden@aidenwallis.co.uk>
2019-11-29 03:54:52 +0000

    Adds another page

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,5 @@
 export const CARD_ID = '49d9ad4b223d1a2';
-export const PAGE_COUNT = 47;
+export const PAGE_COUNT = 48;
 export const BACKGROUND_IMAGE = require('../images/table-background.jpg');
 export const CARD_PAGE = (page) => `https://groupgreeting.storage.googleapis.com/cards/${CARD_ID}/pages/${CARD_ID}_${page}.png?t=${Date.now()}`;
 export const FRONT_IMAGE = require('../images/front-cover.png');
